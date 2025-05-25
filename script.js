fad6f3f263859c2eca63ae9ffac6e9dc63846d84
Tranquil Tortoise <SlinkTheTranquilTortoise@gmail.com>
2025-05-24 21:26:35 -1000

    complete playGame function

--- script.js
+++ script.js
@@ -16,13 +16,69 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     let humanChoice = prompt("Do you choose rock, paper, or scissors?")
+    humanChoice = humanChoice.toLowerCase();
 
     return humanChoice;
 }
 
 function playRound(humanChoice, computerChoice) {
-    
+
+    if (humanChoice === "rock") {
+        if (computerChoice === "rock") {
+            console.log("It's a Tie!");
+            return;
+        }
+        if (computerChoice === "paper") {
+            console.log("You lose! Paper covers Rock!");
+            computerScore++;
+            return;
+        }
+        if (computerChoice === "scissors") {
+            console.log("You win! Rock breaks Scissors!");
+            humanScore++;
+            return;
+        }
+        console.log("Error");
+        return;
+    }
+
+    if (humanChoice === "paper") {
+        if (computerChoice === "rock") {
+            console.log("You win! Paper covers Rock!");
+            humanScore++;
+            return;
+        }
+        if (computerChoice === "paper") {
+            console.log("It's a Tie!");
+            return;
+        }
+        if (computerChoice === "scissors") {
+            console.log("You lose! Scissors cut Paper!");
+            computerScore++;
+            return;
+        }
+        console.log("Error");
+        return;
+    }
+
+    if (humanChoice === "scissors") {
+        if (computerChoice === "rock") {
+            console.log("You lose! Rock breaks Scissors!");
+            computerScore++;
+            return;
+        }
+        if (computerChoice === "paper") {
+            console.log("You win! Scissors cut Paper!");
+            humanScore++;
+            return;
+        }
+        if (computerChoice === "scissors") {
+            console.log("It's a Tie!");
+            return;
+        }
+        console.log("Error");
+        return;
+    }
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+playRound(getHumanChoice(), getComputerChoice());
